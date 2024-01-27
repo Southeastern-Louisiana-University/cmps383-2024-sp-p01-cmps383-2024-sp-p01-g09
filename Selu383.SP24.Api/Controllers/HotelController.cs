05ce8a3bdfec86b30ef976af2b6e6b3774ca116c
Jake Barrios <jake.barrios@selu.edu>
2024-01-27 15:05:22 -0600

    Created the controller file, and DTO and entity.

---
+++ Selu383.SP24.Api/Controllers/HotelController.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using Selu383.SP24.Api.Entities;
+using Selu383.SP24.Api.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+
+
+namespace Selu383.SP24.Api.Controllers
+{
+    [ApiController]
+    [Route("api/hotels")]
+    public class HotelController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+        public HotelController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+
+        
+    }
+}
---
+++ Selu383.SP24.Api/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Selu383.SP24.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+
+
+namespace Selu383.SP24.Api.Data
+{
+    public sealed class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder  modelBuilder)
+        {
+
+        }
+    }
